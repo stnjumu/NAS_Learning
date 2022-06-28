54b92b1e8662a16b23804631afec049002cc3c52
stnjumu <1282807292@qq.com>
2022-06-28 14:59:15 +0800

    init project

---
+++ libs/config_utils/__init__.py
@@ -0,0 +1,5 @@
+##################################################
+# Copyright (c) Xuanyi Dong [GitHub D-X-Y], 2019 #
+##################################################
+# general config related functions
+from .config_utils import load_config, dict2config, configure2str
---
+++ libs/procedures/__init__.py
@@ -0,0 +1,11 @@
+##################################################
+# Copyright (c) Xuanyi Dong [GitHub D-X-Y], 2019 #
+######################################################################
+# This folder is deprecated, which is re-organized in "xalgorithms". #
+######################################################################
+from .starts import prepare_seed
+from .starts import prepare_logger
+from .starts import get_machine_info
+from .starts import save_checkpoint
+from .starts import copy_checkpoint
+from .optimizers import get_optim_scheduler
